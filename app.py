cc13ceb0fa4168525e91422003c0531a3c508bc6
unkind58 <leo.lapenas88@gmail.com>
2020-03-19 20:33:40 +0200

    ver[0.1]. Added staticmethod

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from random import random
+from random import randint
 
 
 class IdNumberGenerator:
-    """"" Class to create ID number according to Lithuanian ID number reglament """
-    def __init__(self, f_name, l_name, gender): 
+    """"" Class to create ID number according to Lithuanian ID number regulation """
+    version = "[0.1]"
+
+    def __init__(self, f_name, l_name, gender): #  f"Hello, {f_name}. You are {age}."
         self.f_name = f_name
         self.l_name = l_name
         self.gender = gender
@@ -14,16 +16,31 @@ class IdNumberGenerator:
         self.month = month
         self.day = day
     
+    #def randomiser(self, last_digits):
+    #    self.last_digits = last_digits
 
-    def randomiser(self):   # ''.join(["{}".format(random.randint(0, 9)) for number in range(0, 10)]) -- randomized for last 4 digits
-        pass
-    
     @classmethod
-    def user_input(cls):
+    def input_user_name(cls):
         return cls(
             input("Please enter your first name: ").title(),
             input("Please enter your last name: ").title(),
             input("Please enter your gender: ").title())
 
-user_id = IdNumberGenerator.user_input()
-print(user_id.f_name)       #  f"Hello, {name}. You are {age}."
+    @staticmethod
+    def create_last_4_digits():
+        last_digits = "".join(["{}".format(randint(0, 9)) for i in range(0, 4)])
+        return last_digits
+
+
+ #   @classmethod
+ #   def input_user_birthday(cls):
+ #       return cls.birthday()
+ #           input("Please enter your year of birth: ").title(),
+ #           input("Please enter your month of birth: ").title(),
+ #           input("Please enter your day of birth: ").title())
+
+
+user_id_name = IdNumberGenerator.input_user_name()
+#user_id_birthday = IdNumberGenerator.input_user_birthday()
+print(user_id_name.f_name)
+print(user_id_name.create_last_4_digits())
