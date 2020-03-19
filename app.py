a8fdba427e6a087be0af458d3b0f64169284671d
unkind58 <leo.lapenas88@gmail.com>
2020-03-19 18:59:43 +0200

    classmethod added

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-import random
+from random import random
 
 
 class IdNumberGenerator:
     """"" Class to create ID number according to Lithuanian ID number reglament """
-    def __init__(self): #  f"Hello, {name}. You are {age}."
-        self.name = input("Please write your name: ")
-        self.id = ''.join(["{}".format(random.randint(0, 9)) for number in range(0, 10)])
+    def __init__(self, f_name, l_name, gender): 
+        self.f_name = f_name
+        self.l_name = l_name
+        self.gender = gender
 
-Participant = Totalizator()
-print(Participant.id)
+
+    def birthday(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+    
+
+    def randomiser(self):   # ''.join(["{}".format(random.randint(0, 9)) for number in range(0, 10)]) -- randomized for last 4 digits
+        pass
+    
+    @classmethod
+    def user_input(cls):
+        return cls(
+            input("Please enter your first name: ").title(),
+            input("Please enter your last name: ").title(),
+            input("Please enter your gender: ").title())
+
+user_id = IdNumberGenerator.user_input()
+print(user_id.f_name)       #  f"Hello, {name}. You are {age}."
