f336360c2fcd3f47a4ba3d37d221fea917c4093b
unkind58 <leo.lapenas88@gmail.com>
2020-03-26 19:36:42 +0200

    Added dockstrings

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import random
 
+""""" Initializing and preparing output answer """
 numbers_in_id = 11
 output_id = [0] * numbers_in_id
-
-class IdNumberGenerator:
-    """"" Class to create ID number according to Lithuanian ID number regulation """
-    version = "[0.5]"
-
-    def __init__(self,):
-        pass
 
 
 class Name:
@@ -36,43 +30,44 @@ class Birthday:
     def input_user_birthday(cls):
         return cls(
             eval(input("Please enter your year of birth: ")),
-            eval(input("Please enter your month of birth: ")),
+            input("Please enter your month of birth: ").title(),
             eval(input("Please enter your day of birth: "))
         )
 
 
+""""" Calling all both Class`es for input """
 user_id = Name.input_user_name()
 user_id_birthday = Birthday.input_user_birthday()
-#print(user_id.f_name)
-#print(user_id_birthday.year)
 
-# ------------- rearranging year -------------
-
+""""" Rearranging year (from 2 to 4 digits) """
 if len(str(user_id_birthday.year)) == 4:
     pass
 if len(str(user_id_birthday.year)) == 2:
-    year = eval(input("Please enter 4 digit year expression: "))
+    user_id_birthday.year = eval(input("Please enter 4 digit year expression: "))
 
-# ------------- 1st digit ------------- [+]
+""""" Rearranging gender (to be Male/Men or Female/Women) """
+if user_id.gender.startswith("M") or user_id.gender.startswith("W") or user_id.gender.startswith("F"):
+    pass
+else:
+    user_id.gender = "".join(input("Please enter Male or Female as your gender: ").split()).title()
 
+""""" 1st digit """
 if len(str(user_id_birthday.year)) == 4 and user_id_birthday.year in range(1900, 1999):
     if user_id.gender.startswith("M"):
         output_id[0] = 3
     else:
         output_id[0] = 4
 if len(str(user_id_birthday.year)) == 4 and user_id_birthday.year in range(2000, 2999):
-    if user_id.gender.startswith("W") and 1900 <= user_id_birthday.year <= 1999:
+    if user_id.gender.startswith("F") or user_id.gender.startswith("W") and 1900 <= user_id_birthday.year <= 1999:
         output_id[0] = 6
     else:
         output_id[0] = 5
 
-# ------------- 2nd-3rd digit ------------- [+]
-
+""""" 2nd-3rd digit """
 output_id[1] = user_id_birthday.year % 100 // 10
 output_id[2] = user_id_birthday.year % 10
 
-# ------------- 4th-5th digit ------------- [+]
-
+""""" 4th-5th digit """
 months = {
     "January": 1,
     "February": 2,
@@ -90,22 +85,21 @@ months = {
 try:
     if int(user_id_birthday.month):
         pass
-except ValueError:
+except ValueError or NameError:
     try:
         if str(user_id_birthday.month):
-            month = months.get(user_id_birthday.month)
-    except ValueError:
-        year = eval(input("Please enter valid month expression: "))
+            user_id_birthday.month = months.get(user_id_birthday.month)
+    except ValueError or NameError:
+        user_id_birthday.month = eval(input("Please enter valid month expression: "))
 
 if len(str(user_id_birthday.month)) == 1:
     output_id[3] = 0
-    output_id[4] = user_id_birthday.month
+    output_id[4] = int(user_id_birthday.month)
 else:
-    output_id[3] = user_id_birthday.month // 10
-    output_id[4] = user_id_birthday.month % 10
+    output_id[3] = int(user_id_birthday.month) // 10
+    output_id[4] = int(user_id_birthday.month) % 10
 
-# ------------- 6th-7th digit ------------- [+]
-
+""""" 6th-7th digit """
 if len(str(user_id_birthday.day)) == 1:
     output_id[5] = 0
     output_id[6] = user_id_birthday.day
@@ -113,14 +107,13 @@ else:
     output_id[5] = user_id_birthday.day // 10
     output_id[6] = user_id_birthday.day % 10
 
-# ------------- 8th-10th digit ------------- [+]
-
+""""" 8th-10th digit """
 sequence = str("".join(["{}".format(random.randint(0, 9)) for i in range(0, 3)]))
 output_id[7] = eval(sequence[0])
 output_id[8] = eval(sequence[1])
 output_id[9] = eval(sequence[2])
 
-# ------------- 11th digit -------------
+""""" 11th digit """
 counter = 1
 last_digit = 0
 for digit in range(len(output_id) - 1):
@@ -128,7 +121,6 @@ for digit in range(len(output_id) - 1):
         counter = 1
     last_digit += output_id[digit] * counter
     counter += 1
-# print(last_digit)
 counter = 3
 if last_digit % 11 != 10:
     output_id[10] = last_digit % 11
@@ -143,9 +135,9 @@ if last_digit % 11 != 10:
     output_id[10] = last_digit % 11
 else:
     output_id[10] = 0
-# print(last_digit)
 
-print(output_id)
+""""" Output """
+print(f"Dear {user_id.f_name} {user_id.l_name}, your ID number is {''.join(map(str, output_id))}")
 
 
 # Additional possible features:
