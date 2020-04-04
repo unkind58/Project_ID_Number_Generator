135577ba1a76c2524a5b92a09159b3a50181c7b2
unkind58 <leo.lapenas88@gmail.com>
2020-04-04 09:36:10 +0300

    Fixed day validaition

--- day_validation.py
+++ day_validation.py
@@ -1,6 +1,6 @@
 def day_valid(day, month):
     days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if 0 < day < days[month]:
+    if 0 < day < days[int(month)]:
         pass
     else:
         day = int(input("Please enter day in numerical expression: "))
